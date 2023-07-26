87fe9d319c4a22ad09665827f0124e15e33a1b65
the-castaway <jccd.designs@gmail.com>
2023-07-26 16:09:59 -0700

    Addition of GSAP plugin

--- src/components/header.react.js
+++ src/components/header.react.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
-//import { SplitText } from "gsap/SplitText";
+import { SplitText } from "gsap/SplitText";
 //styles
 import '../styles/header.css';
 import '../styles/App.css';
 
 const Header = ({ headerContent }) => {
-  //gsap.registerPlugin(SplitText);
+  gsap.registerPlugin(SplitText);
   let header = useRef(null);
+  let quote = useRef(null);
 
 
 
@@ -18,11 +19,24 @@ const Header = ({ headerContent }) => {
 
   useEffect(() => {
 
-    // var tl = gsap.timeline(),
-    //   mySplitText = new SplitText(header, { type: "words,chars" }),
-    //   chars = mySplitText.chars; //an array of all the divs that wrap each character
 
-    // console.log(chars);
+    console.log(header);
+    var tl = gsap.timeline(),
+      mySplitText = new SplitText(header, { type: "words,chars" }),
+      chars = mySplitText.chars; //an array of all the divs that wrap each character
+
+    console.log(chars);
+
+    tl.from(chars, {
+      duration: 0.8,
+      opacity: 0,
+      //scale: 0,
+      y: 200,
+      rotationX: 0,
+      // transformOrigin: "0% -50% 50",
+      ease: "ease",
+      stagger: 0.1
+    });
 
 
     gsap.fromTo(
@@ -40,11 +54,15 @@ const Header = ({ headerContent }) => {
 
 
   return (
-    <h1 className='header'>
-      <div ref={el => header = el}>
-        {headerContent}
-      </div>
-    </h1>
+
+    <div className='header-container'>
+      <h1 className='header'>
+        <div ref={el => header = el}>
+          {headerContent}
+        </div>
+      </h1>
+    </div>
+
   );
 }
 
