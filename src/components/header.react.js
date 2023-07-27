b1b2dffed8d575ed2b4a39cf6f7a159e7dea06f6
the-castaway <jccd.designs@gmail.com>
2023-07-26 20:11:49 -0700

    Revert "Loader Fix"
    This reverts commit f55e75629bc4c6c5eed99298b0e27139ca8db178.

--- src/components/header.react.js
+++ src/components/header.react.js
@@ -8,6 +8,7 @@ import '../styles/App.css';
 const Header = ({ headerContent }) => {
   gsap.registerPlugin(SplitText);
   let header = useRef(null);
+  let quote = useRef(null);
 
 
 
@@ -18,10 +19,13 @@ const Header = ({ headerContent }) => {
 
   useEffect(() => {
 
+
+    console.log(header);
     var tl = gsap.timeline(),
       mySplitText = new SplitText(header, { type: "words,chars" }),
       chars = mySplitText.chars; //an array of all the divs that wrap each character
 
+    console.log(chars);
 
     tl.from(chars, {
       duration: 0.8,
@@ -35,6 +39,17 @@ const Header = ({ headerContent }) => {
     });
 
 
+    gsap.fromTo(
+      header,
+      {
+        y: 50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+      },
+    )
   }, [header])
 
 
--- src/pages/loader.js
+++ src/pages/loader.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { gsap } from "gsap";
 //components
 import Header from '../components/header.react';
@@ -6,16 +6,7 @@ import Header from '../components/header.react';
 import '../styles/loader.css';
 
 const Loader = () => {
-    let loaderInfo1 = useRef(null);
-    let loaderInfo2 = useRef(null);
-    let loaderInfo3 = useRef(null);
 
-    useEffect(() => {
-        var tl = gsap.timeline();
-        tl.from(loaderInfo1, { delay: 1, opacity: 0, duration: 0.8 })
-        tl.from(loaderInfo2, { opacity: 0, duration: 0.8 })
-        tl.from(loaderInfo3, { opacity: 0, duration: 0.8 })
-    })
 
     return (
         <div id="loader" className='loader'>
@@ -23,17 +14,17 @@ const Loader = () => {
                 <Header headerContent="100%" />
             </div>
             <div className='loader-info'>
-                <div ref={el => loaderInfo1 = el} className='loader-info-left'>
+                <div className='loader-info-left'>
                     <h2>
                         Jaime Castaneda
                     </h2>
                 </div>
-                <div ref={el => loaderInfo2 = el} className='loader-info-center'>
+                <div className='loader-info-center'>
                     <h2>
                         Designer + Developer
                     </h2>
                 </div>
-                <div ref={el => loaderInfo3 = el} className='loader-info-right'>
+                <div className='loader-info-right'>
                     <h2>
                         Portfolio ©2023
                     </h2>
