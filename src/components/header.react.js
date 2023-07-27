bd9b674fe41832ad434ef46d4b5ded86816b59d3
the-castaway <jccd.designs@gmail.com>
2023-07-26 21:38:44 -0700

    Minor Fixes for GSAP

--- src/components/header.react.js
+++ src/components/header.react.js
@@ -8,48 +8,22 @@ import '../styles/App.css';
 const Header = ({ headerContent }) => {
   gsap.registerPlugin(SplitText);
   let header = useRef(null);
-  let quote = useRef(null);
-
-
-
-
-
-
-
 
   useEffect(() => {
 
-
-    console.log(header);
     var tl = gsap.timeline(),
       mySplitText = new SplitText(header, { type: "words,chars" }),
       chars = mySplitText.chars; //an array of all the divs that wrap each character
 
-    console.log(chars);
-
     tl.from(chars, {
       duration: 0.8,
       opacity: 0,
-      //scale: 0,
       y: 200,
       rotationX: 0,
-      // transformOrigin: "0% -50% 50",
       ease: "ease",
       stagger: 0.1
     });
 
-
-    gsap.fromTo(
-      header,
-      {
-        y: 50,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-      },
-    )
   }, [header])
 
 
