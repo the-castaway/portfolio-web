6b96085eb1b709bbb945c6b712fecfc04879c720
the-castaway <jccd.designs@gmail.com>
2023-07-26 20:22:29 -0700

    Animation Fix

--- src/pages/loader.js
+++ src/pages/loader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { gsap } from "gsap";
 //components
 import Header from '../components/header.react';
@@ -6,7 +6,16 @@ import Header from '../components/header.react';
 import '../styles/loader.css';
 
 const Loader = () => {
+    let loaderInfo1 = useRef(null);
+    let loaderInfo2 = useRef(null);
+    let loaderInfo3 = useRef(null);
 
+    useEffect(() => {
+        var tl = gsap.timeline();
+        tl.from(loaderInfo1, { delay: 1, opacity: 0, duration: 0.8 })
+        tl.from(loaderInfo2, { opacity: 0, duration: 0.8 })
+        tl.from(loaderInfo3, { opacity: 0, duration: 0.8 })
+    })
 
     return (
         <div id="loader" className='loader'>
@@ -14,17 +23,17 @@ const Loader = () => {
                 <Header headerContent="100%" />
             </div>
             <div className='loader-info'>
-                <div className='loader-info-left'>
+                <div ref={el => loaderInfo1 = el} className='loader-info-left'>
                     <h2>
                         Jaime Castaneda
                     </h2>
                 </div>
-                <div className='loader-info-center'>
+                <div ref={el => loaderInfo2 = el} className='loader-info-center'>
                     <h2>
                         Designer + Developer
                     </h2>
                 </div>
-                <div className='loader-info-right'>
+                <div ref={el => loaderInfo3 = el} className='loader-info-right'>
                     <h2>
                         Portfolio ©2023
                     </h2>
