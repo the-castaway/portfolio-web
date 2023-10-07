23976128f835a51506a430d2c12213d6fca501a1
the-castaway <jccd.designs@gmail.com>
2023-10-07 12:23:12 -0700

    Modal

--- src/components/nav.react.js
+++ src/components/nav.react.js
@@ -8,21 +8,25 @@ import '../styles/nav.css';
 const Nav = ({ location }) => {
     const pathname = location.pathname;
     const [previousPath, setPreviousPath] = useState(pathname);
+    const [collapsed, setCollapsed] = useState(true)
     gsap.registerPlugin(SplitText);
+    //nav refs
     let nav = useRef(null);
-    // title timeline refs
+    //title timeline refs
     let title_move = useRef(null);
     let title_fade_1 = useRef(null);
     let title_fade_2 = useRef(null);
-    // social timeline refs
+    //social timeline refs
     let social_fade = useRef(null);
-    // link timeline refs
+    //link timeline refs
     let link_container = useRef(null);
     let link_1 = useRef(null);
     let link_2 = useRef(null);
     let link_3 = useRef(null);
     let link_expand = useRef(null);
-
+    let link_expand_line = useRef(null);
+    //modal refs
+    let modal = useRef(null);
 
     useLayoutEffect(() => {
         if (["/"].includes(pathname) && pathname !== previousPath) {
@@ -151,44 +155,115 @@ const Nav = ({ location }) => {
         setPreviousPath(location.pathname);
     }, [pathname, previousPath]);
 
+    const expandModal = () => {
+        setCollapsed(false);
+        let ctx = gsap.context(() => {
+            const modal_tl = gsap.timeline();
+            modal_tl.to(modal, {
+                duration: 1,
+                opacity: 1,
+                display: 'flex',
+            }, 0)
+            modal_tl.to(link_expand, {
+                duration: 0.4,
+                rotate: 0,
+                opacity: 1,
+                display: 'flex',
+
+            }, 0)
+            modal_tl.to(link_expand_line, {
+                duration: 0.4,
+                rotate: -90,
+                opacity: 1,
+                display: 'flex',
+            }, 0)
+        })
+    }
+
+    const collapseModal = () => {
+        if (collapsed === false) {
+            setCollapsed(true);
+            let ctx = gsap.context(() => {
+                const modal_tl = gsap.timeline();
+                modal_tl.to(modal, {
+                    duration: 1,
+                    opacity: 0,
+                    display: 'none',
+                }, 0)
+                modal_tl.to(link_expand, {
+                    duration: 0.4,
+                    rotate: 90,
+                    opacity: 1,
+                    display: 'flex',
+                }, 0)
+                modal_tl.to(link_expand_line, {
+                    duration: 0.4,
+                    rotate: 0,
+                    opacity: 1,
+                    display: 'flex',
+                }, 0)
+
+            })
+        }
+    }
+
+    const toggleModal = () => {
+        if (collapsed) {
+            expandModal();
+        }
+        else {
+            collapseModal();
+        }
+    }
+
     return (
-        <nav ref={el => nav = el} className='nav'>
-            <NavLink to="/" exact='true' className='nav-title'>
-                <div>J</div>
-                <div ref={el => title_fade_1 = el}>aime</div>
-                <div ref={el => title_move = el}>C</div>
-                <div ref={el => title_fade_2 = el}>astaneda</div>
-            </NavLink>
-            <div className="nav-social" ref={el => social_fade = el}>
-                <a href="https://www.linkedin.com/in/jaime-castaneda-956154a8/">
-                    Linkedin
-                </a>
-                <a href="https://www.instagram.com/the_casta_way/">
-                    Instagram
-                </a>
-                <a href="https://twitter.com/the_casta_way">
-                    Threads
-                </a>
-            </div>
-            <div className="nav-links">
-                <div ref={el => link_container = el} className="nav-links-border" />
-                <div className="nav-links-content">
-                    <NavLink ref={el => link_1 = el} to="/showcase" exact='true'>
-                        Showcase
-                    </NavLink>
-                    <NavLink ref={el => link_2 = el} to="/about" exact='true'>
-                        About
-                    </NavLink>
-                    <a ref={el => link_3 = el} href="mailto:jccd.designs@gmail.com">
-                        Contact
+        <>
+            <nav ref={el => nav = el} className='nav'>
+                <NavLink onClick={() => { collapseModal(); }} to="/" exact='true' className='nav-title'>
+                    <div>J</div>
+                    <div ref={el => title_fade_1 = el}>aime</div>
+                    <div ref={el => title_move = el}>C</div>
+                    <div ref={el => title_fade_2 = el}>astaneda</div>
+                </NavLink>
+                <div className="nav-social" ref={el => social_fade = el}>
+                    <a href="https://www.linkedin.com/in/jaime-castaneda-956154a8/">
+                        Linkedin
+                    </a>
+                    <a href="https://www.instagram.com/the_casta_way/">
+                        Instagram
+                    </a>
+                    <a href="https://twitter.com/the_casta_way">
+                        Threads
                     </a>
                 </div>
-                <div ref={el => link_expand = el} className="nav-button-expand">
-                    <div className="nav-button-cross-horizontal" />
-                    <div className="nav-button-cross-vertical" />
+                <div className="nav-links">
+                    <div ref={el => link_container = el} className="nav-links-border" />
+                    <div className="nav-links-content">
+                        <NavLink ref={el => link_1 = el} to="/showcase" exact='true'>
+                            Showcase
+                        </NavLink>
+                        <NavLink ref={el => link_2 = el} to="/about" exact='true'>
+                            About
+                        </NavLink>
+                        <a ref={el => link_3 = el} href="mailto:jccd.designs@gmail.com">
+                            Contact
+                        </a>
+                    </div>
+                    <div ref={el => link_expand = el} onClick={() => { toggleModal(); }} className="nav-button-expand">
+                        <div className="nav-button-cross-horizontal" />
+                        <div ref={el => link_expand_line = el} className="nav-button-cross-vertical" />
+                    </div>
                 </div>
+            </nav>
+            <div ref={el => modal = el} className="nav-modal">
+                <ul>
+                    <li><h2>work</h2></li>
+                    <li><h2>about</h2></li>
+                    <li><h2>hello@jaimecastaneda.com</h2></li>
+                    <li><h2>+1.408.828.3020</h2></li>
+                </ul>
             </div>
-        </nav>
+        </>
     );
 }
 
