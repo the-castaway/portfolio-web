81342f15f734578a8648d3f94fd4e8cb91ea925a
the-castaway <jccd.designs@gmail.com>
2024-02-04 22:40:48 -0800

    Link Cleanup

--- src/components/link.react.js
+++ src/components/link.react.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, useMemo } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState, useMemo } from 'react';
 import { gsap } from 'gsap';
 //styles
 import '../styles/link.css';
@@ -7,17 +7,17 @@ import '../styles/App.css';
 const Link = ({ header, description }) => {
     const mediaQuery = useMemo(() => window.matchMedia(`(max-width: 600px)`), [`(max-width: 600px)`])
     const [matches, setMatches] = useState(mediaQuery.matches);
-    let link = useRef(null);
-    let linkIcon = useRef(null);
-    let linkIconBorder = useRef(null);
-    let linkIconText = useRef(null);
-    let linkIconVector = useRef(null);
+    const link = useRef(HTMLElement);
+    const linkIcon = useRef(HTMLElement);
+    const linkIconBorder = useRef(HTMLElement);
+    const linkIconText = useRef(HTMLElement);
+    const linkIconVector = useRef(HTMLElement);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         const linkTL = gsap.timeline();
         linkTL.pause();
         linkTL.fromTo(
-            linkIcon,
+            linkIcon.current,
             {
                 duration: 0.3,
                 width: () => { return matches ? 35 : 58 },
@@ -29,19 +29,19 @@ const Link = ({ header, description }) => {
                 ease: "ease",
             },
             0);
-        linkTL.to(linkIconVector, {
+        linkTL.to(linkIconVector.current, {
             duration: 0.3,
             rotate: "45deg",
             ease: "ease",
         }, 0);
-        linkTL.to(linkIconText, {
+        linkTL.to(linkIconText.current, {
             duration: 0.3,
             opacity: 1,
             ease: "ease",
             delay: 0.2,
         }, 0);
-        link.addEventListener('mouseenter', () => { linkTL.play(); })
-        link.addEventListener('mouseleave', () => { linkTL.reverse(); })
+        link.current.addEventListener('mouseenter', () => { linkTL.play(); })
+        link.current.addEventListener('mouseleave', () => { linkTL.reverse(); })
     }, [matches]);
 
 
@@ -55,20 +55,20 @@ const Link = ({ header, description }) => {
 
 
     return (
-        <div className='link' ref={el => link = el}>
+        <div className='link' ref={link}>
             <div className='link-header'>
                 {header}
             </div>
-            <div ref={el => linkIcon = el} className="link-icon" style={{ width: matches ? 35 : 58 }}>
-                <div ref={el => linkIconBorder = el} className="link-icon-border" />
-                <div ref={el => linkIconVector = el} className="link-icon-vector">
+            <div ref={linkIcon} className="link-icon" style={{ width: matches ? 35 : 58 }}>
+                <div ref={linkIconBorder} className="link-icon-border" />
+                <div ref={linkIconVector} className="link-icon-vector">
                     <svg height={matches ? 10 : 20} width={matches ? 10 : 20} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.03906 1.05957H18.9378V18.9571" stroke="#ECECEC" strokeWidth="2" strokeMiterlimit="10" />
                         <path d="M1.03906 18.9571L18.9378 1.05957" stroke="#ECECEC" strokeWidth="2" strokeMiterlimit="10" />
                     </svg>
                 </div>
                 <div>
-                    <p ref={el => linkIconText = el} className="link-icon-text">
+                    <p ref={linkIconText} className="link-icon-text">
                         {description}
                     </p>
                 </div>
