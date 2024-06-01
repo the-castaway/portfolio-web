d87f47c2b03c7e375284186db0f39e3933eaf276
the-castaway <jccd.designs@gmail.com>
2024-06-01 00:58:40 -0700

    [WIP] Addition of figma link

--- src/components/wip.react.js
+++ src/components/wip.react.js
@@ -5,7 +5,8 @@ import '../styles/wip.css';
 const WIP = () => {
     return (
         <div className='wip'>
-            WIP 😅
+            <p>
+                WIP 😅 Check out my <a href="https://www.figma.com/design/cuwOkDGYFd4LJqDwgXFIJU/Site?node-id=0-1&t=mbMF96b7c0ItH1GW-1">Figma</a></p>
         </div >
     );
 }
