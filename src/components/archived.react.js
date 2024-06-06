6ddc7c6f3036eb84a7574ea1ff6ecee6917d355d
the-castaway <jccd.designs@gmail.com>
2024-06-05 19:11:39 -0700

    [ARCHIVE] Page structure

---
+++ src/components/archived.react.js
@@ -0,0 +1,88 @@
+import { React, useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import {
+    Link
+} from "react-router-dom"
+//styles
+import '../styles/archived.css';
+
+const Archived = ({ children, name, href }) => {
+    //refs
+    const archived = useRef(HTMLElement);
+    const archivedName = useRef(HTMLElement);
+    const archivedHover = useRef(HTMLElement);
+
+    //intro 
+    useLayoutEffect(() => {
+        const ctx = gsap.context((context) => {
+            const tl = gsap.timeline();
+            tl.to(archived.current, {
+                height: 'auto',
+                duration: 0.5,
+                ease: 'ease',
+            }, 0).to(archivedName.current, {
+                color: "#4552D9",
+                duration: 0.5,
+                ease: 'ease',
+            }, 0).to(archivedHover.current, {
+                opacity: 1,
+                duration: 0.5,
+                ease: 'ease',
+                delay: 0.2,
+            }, 0);
+            tl.pause();
+
+            context.add('handleMouseEnter', () => {
+                tl.play();
+                console.log('play')
+            })
+
+            context.add('handleMouseLeave', () => {
+                tl.reverse();
+                console.log('reverse')
+            })
+        })
+        archived.current.addEventListener('mouseenter', ctx.handleMouseEnter);
+        archived.current.addEventListener('mouseleave', ctx.handleMouseLeave);
+        return () => {
+            archived.current.removeEventListener('mouseenter', ctx.handleMouseEnter);
+            archived.current.removeEventListener('mouseleave', ctx.handleMouseLeave);
+            ctx.revert();
+        };
+    }, [])
+
+    return (
+        <Link ref={archived} to={href} className='archived'>
+            <div className='archived-content'>
+                <div className='archived-info'>
+                    <div className='archived-info-name'>
+                        <h4>
+                            Name
+                        </h4>
+                        <p ref={archivedName}>
+                            {name}
+                        </p>
+                    </div>
+                </div>
+                <div className='archived-icon'>
+                    <div className="archived-icon-vector">
+                        <svg height={20} width={20} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.03906 1.05957H18.9378V18.9571" stroke="#ECECEC" strokeWidth="2" strokeMiterlimit="10" />
+                            <path d="M1.03906 18.9571L18.9378 1.05957" stroke="#ECECEC" strokeWidth="2" strokeMiterlimit="10" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div ref={archivedHover} className='archived-hover'>
+                <div className='archived-hover-description'>
+                    <h4>
+                        Description
+                    </h4>
+                    {children}
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default Archived;
--- src/pages/archive/archive.js
+++ src/pages/archive/archive.js
@@ -4,6 +4,8 @@ import { gsap } from 'gsap';
 import { SplitText } from "gsap/SplitText";
 //components
 import Footer from '../../components/footer.react';
+import CTA from '../../components/cta.react';
+import Archived from '../../components/archived.react';
 //styles
 import '../../styles/archive.css';
 //assets
@@ -16,12 +18,116 @@ const Archive = () => {
         <div className='archive'>
             <div className='archive-container'>
                 <div className='archive-content'>
-                    <h1>
-                        Archive
-                    </h1>
+                    <div className='archive-preview-container'>
+                        <h1 className='archive-preview-header'>
+                            Archive
+                        </h1>
+                        <div className='archive-preview'>
+                            <img className='about-headshot' key={Media[0].key} src={Media[0].src} />
+                        </div>
+                        <div className='archive-preview-info'>
+                            <h2>
+                                <b>PR. XXX <br />/ 016</b>
+                            </h2>
+                        </div>
+                    </div>
+                    <div className='archive-project-container'>
+                        <div className='archive-project-list'>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+                            <Archived name={'Project Name'} href={'/showcase'}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui.
+                                </p>
+                            </Archived>
+
+
+                        </div>
+
+                    </div>
+
+
                 </div>
             </div>
-
+            <CTA />
             <Footer instruction={"Scroll Down"} scroll={true} />
         </div >
     );
