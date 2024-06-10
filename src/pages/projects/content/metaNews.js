bff9ccffbc8d3d1d5aba2518bff9f3329b6f157c
the-castaway <jccd.designs@gmail.com>
2024-06-09 23:06:58 -0700

    [CURSOR] Hover on content link

--- src/pages/projects/content/metaNews.js
+++ src/pages/projects/content/metaNews.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 //components
 import Links from '../../../components/link.react';
 //styles
@@ -20,7 +21,9 @@ const MetaNews = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut facilisis arcu, eget commodo dui. Morbi eget sapien volutpat, tristique purus semper, ullamcorper orci. Nullam at neque mauris. Phasellus a mollis magna, sed viverra sapien. Morbi luctus efficitur tellus, ac suscipit nibh lacinia dapibus. Nullam cursus tempor dolor, a tristique orci imperdiet venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </p>
             <div className='content-next'>
-                <Links header={'Next'} description={"Let's explore"} />
+                <Link to={"/showcase"}>
+                    <Links header={'Next'} description={"Let's explore"} />
+                </Link>
             </div>
         </div>
     );
