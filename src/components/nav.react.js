704423a511741062358bc760023e9b9b80d82e1c
the-castaway <jccd.designs@gmail.com>
2024-06-10 15:16:09 -0700

    [ABOUT][LAB] Deactivate links until ready

--- src/components/nav.react.js
+++ src/components/nav.react.js
@@ -153,12 +153,19 @@ const Nav = () => {
                     <NavLink ref={navModalShowcase} onClick={() => { collapseModal(); }} to="/showcase" exact='true'>
                         <Link header={'Showcase'} description={"Let's get it"} />
                     </NavLink>
-                    <NavLink ref={navModalAbout} onClick={() => { collapseModal(); }} to="/about" exact='true'>
+                    {/* <NavLink ref={navModalAbout} onClick={() => { collapseModal(); }} to="/about" exact='true'>
                         <Link header={'About'} description={"Who is Jaime?"} />
                     </NavLink>
                     <NavLink ref={navModalLab} onClick={() => { collapseModal(); }} to="/lab" exact='true'>
                         <Link header={'Lab'} description={"Let's explore"} />
-                    </NavLink>
+                    </NavLink> */}
+                    <div ref={navModalAbout} className="nav-modal-link-inactive">
+                        <Link header={'About'} description={"Soon"} />
+                    </div>
+                    <div ref={navModalLab} className="nav-modal-link-inactive">
+                        <Link header={'Lab'} description={"Soon"} />
+                    </div>
+
                 </div>
                 <div className="nav-modal-contact" ref={navModalContact} >
                     <h2 className="nav-modal-contact-header">
